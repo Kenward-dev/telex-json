18f5bafddebaec0612f45872dfe8f607e9048cd5
Kenward Terhemba <codewithkenward@gmail.com>
2025-02-19 13:31:25 +0100

    update:modified the configuration json endpoint to root endpoint

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/integration-json")
+@app.get("/")
 def get_integration_json():
     with open('integration.json', 'r') as file:
         integration_json = json.load(file)
