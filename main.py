3cbfacf77cfddcc624973bdb198d4a9857ac412f
Kenward Terhemba <codewithkenward@gmail.com>
2025-02-20 21:28:29 +0100

    update:added cors configurations

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import json
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["POST", "OPTIONS"],  
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def get_integration_json():
